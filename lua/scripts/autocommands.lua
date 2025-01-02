79083225661f6e615206fdbdb4b6b3d4c70a861a
Rochumback <victor.rochumback@proton.me>
2025-01-01 23:25:18 -0300

    Set tabs instead of spaces and some bling

--- lua/scripts/autocommands.lua
+++ lua/scripts/autocommands.lua
@@ -5,9 +5,34 @@ vim.api.nvim_create_autocmd("BufEnter", {
     local api = require("nvim-tree.api")
     vim.keymap.set('n', 'l', api.node.open.edit)
 end})
-
 vim.api.nvim_create_autocmd("BufLeave", {
     pattern = "NvimTree*",
     callback = function ()
     pcall(vim.cmd, "unmap l")
 end})
+
+vim.api.nvim_create_augroup("RetabSettings", {clear = true })
+vim.api.nvim_create_autocmd("BufReadPost", {
+    group = "RetabSettings",
+    pattern = "*",
+    callback = function ()
+        vim.opt_local.expandtab = false
+        vim.cmd("retab! 4")
+    end
+})
+vim.api.nvim_create_autocmd("BufWritePre", {
+    group = "RetabSettings",
+    pattern = "*",
+    callback = function ()
+        vim.opt_local.expandtab = true
+        vim.cmd("retab! 4")
+    end
+})
+vim.api.nvim_create_autocmd("BufWritePost", {
+    group = "RetabSettings",
+    pattern = "*",
+    callback = function ()
+        vim.opt_local.expandtab =false
+        vim.cmd("retab! 4")
+    end
+})
--- lua/scripts/config.lua
+++ lua/scripts/config.lua
@@ -7,8 +7,14 @@ vim.diagnostic.config({ signs = false, })
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
+vim.opt.tabstop = 8
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.scrolloff = 12
+
+vim.opt.undofile = true
+
+vim.opt.list = true
+vim.opt.listchars = [[tab:◦◦•,space: ,nbsp:␣,trail:•,eol:↴,precedes:«,extends:»]]
+vim.api.nvim_set_hl(0, "NonText", { bg = "white"} )
