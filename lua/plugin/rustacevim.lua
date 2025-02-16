5b21e63df94a089448d1d20a46680895bd2da29e
Rochumback <victor.rochumback@proton.me>
2025-02-16 14:28:19 -0300

    remove old code

--- lua/plugin/rustacevim.lua
+++ lua/plugin/rustacevim.lua
@@ -1,18 +1,3 @@
--- return {
---     "simrat39/rust-tools.nvim",
---     lazy = false,
---     config = function()
---         local rust_tools = require("rust-tools")
---         rust_tools.setup({
---             server = {
---                 -- on_attach = function(_, bufnr)
---                 --
---                 -- end
---             }
---         })
---     end
--- }
---
 return {
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
