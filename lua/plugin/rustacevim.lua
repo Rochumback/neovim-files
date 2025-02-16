6e8d7606ac7a1e5a2d3422ae1d888d44704de65d
Rochumback <victor.rochumback@proton.me>
2025-02-16 14:27:46 -0300

    rename rust tools module

---
+++ lua/plugin/rustacevim.lua
@@ -0,0 +1,21 @@
+-- return {
+--     "simrat39/rust-tools.nvim",
+--     lazy = false,
+--     config = function()
+--         local rust_tools = require("rust-tools")
+--         rust_tools.setup({
+--             server = {
+--                 -- on_attach = function(_, bufnr)
+--                 --
+--                 -- end
+--             }
+--         })
+--     end
+-- }
+--
+return {
+    'mrcjkb/rustaceanvim',
+    version = '^5', -- Recommended
+    lazy = false,   -- This plugin is already lazy
+    config = function() end
+}
